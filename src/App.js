6686937c534c284a7ec68d0290a722b870be9629
fmontoy <fmontoyalvarez@gmail.com>
2019-02-11 22:14:48 -0500

    Creando primera base del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Modal from './components/UI/Modal/Modal';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Wolox Technical test
         </header>
+        <Modal>
+          <h1>Probando</h1>
+          <p><b>Estoy Viendo como se ve esto</b></p>
+        </Modal>
       </div>
     );
   }
